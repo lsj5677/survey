55797deb0a6ee754499ec1fa429a85dd81626920
lsj5677 <lsj5677@naver.com>
2022-07-26 19:58:23 +0900

    5

--- gen-firebase-config.js
+++ gen-firebase-config.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
 
-console.log(process.env.SURVEY_FIREBASE_CLIENT_PRIVATE_KEY);
 if (!fs.existsSync('config')) fs.mkdirSync('config');
 if (!fs.existsSync('config/firebaseConfig.json')) fs.writeFileSync('config/firebaseConfig.json', `${process.env.SURVEY_FIREBASE_CLIENT_PRIVATE_KEY}`);
